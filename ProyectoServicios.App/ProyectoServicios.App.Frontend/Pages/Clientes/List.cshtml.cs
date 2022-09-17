fc58d776a9b47f10698771d9346deffa959708f1
JhomaroGC <empresariar@gmail.com>
2022-09-16 20:13:32 -0500

    v capa Frontend

---
+++ ProyectoServicios.App/ProyectoServicios.App.Frontend/Pages/Clientes/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoServicios.App.Frontend.Pages
+{
+    public class ListModelClientes : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
